c8184c0f76b624b8df53ba5ca4ac3e7b2bb44049
Bo Lin <38364537+BoLin@users.noreply.github.com>
2018-05-31 18:07:07 -0500

    Update removeDuplicates.py

--- leetcode/removeDuplicates.py
+++ leetcode/removeDuplicates.py
@@ -5,12 +5,12 @@ class Solution:
         :rtype: int
         """
         i = 0
-        while i < len(A)-1: ##此处索引一直更新所以不会超限
-            if A[i] == A[i+1]:
+        while i < len(nums)-1: ##此处索引一直更新所以不会超限
+            if nums[i] == nums[i+1]:
                 A.remove(A[i])
             else:
                 i += 1
-        return len(A)
+        return len(nums)
     
     ## 如果用for loop会有问题，此处的i 循环不更新，所以会索引超限
         for i in range(len(nums)-1):
