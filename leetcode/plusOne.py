82cb22ea721b325664160090516fb1c94940358e
Bo Lin <38364537+BoLin@users.noreply.github.com>
2018-06-02 22:33:53 -0500

    Update plusOne.py

--- leetcode/plusOne.py
+++ leetcode/plusOne.py
@@ -1,9 +1,10 @@
-class Solution:
 ##Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
 
-The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+##The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
 
-You may assume the integer does not contain any leading zero, except the number 0 itself.##
+##You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+class Solution:
     def plusOne(self, digits):
         """
         :type digits: List[int]
