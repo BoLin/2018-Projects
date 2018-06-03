0959cd82ab8f75d92e244647187541e45a81d0cb
Bo Lin <38364537+BoLin@users.noreply.github.com>
2018-06-02 22:33:23 -0500

    Create plusOne.py

---
+++ leetcode/plusOne.py
@@ -0,0 +1,26 @@
+class Solution:
+##Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.##
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        digits.insert(0,0)
+        l = len(digits) 
+                
+        for i in reversed(range(l)):
+            if digits[i]+1<=9 :
+                digits[i] +=1
+                break
+            else:
+                digits[i]=0
+        
+        if digits[0]==0:
+            digits.remove(0)
+            
+        return digits
+            
