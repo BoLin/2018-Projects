21ae512f1ec8592b4a0094bef13f8d19d85f8e83
Bo Lin <38364537+BoLin@users.noreply.github.com>
2018-09-07 11:42:42 +0800

    Update manual_validation_svm.py

--- AI-Sales/manual_validation_svm.py
+++ AI-Sales/manual_validation_svm.py
@@ -6,20 +6,24 @@ from sklearn import svm
 import dataloader
 
 X, Y = dataloader.test()
-clf = svm.SVC()
-#clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(62,53,21,16), random_state=1)
-clf.fit(X, Y)
-Y_predict = clf.predict(X)
-Y_predict = Y_predict.tolist()
 
-for i in range(len(Y[0])):
+#single class training
+for k in range(len(Y[0])):
+    Y_single = []
+    for i in Y:
+        Y_single.append(i[k])
+    test = svm.SVC()
+    #clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(62,53,21,16), random_state=1)
+    test.fit(X, Y_single)
+    Y_predict = test.predict(X)
     correct = 0
     wrong = 0
-    for j in range(len(Y)):
-        if Y[j][i] == Y_predict[j][i]:
-            correct += 1
-        else:
+    for j in range(len(Y_single)):
+        if Y_single[j] != Y_predict[j]:
             wrong += 1
-    print(i)
+        else:
+            correct +=1
+    print(k)
     print(correct/(correct + wrong))
-    print("================")
+    print("=========")
+
