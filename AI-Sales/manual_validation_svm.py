d3828f8389e8940d9422f0a7a442af66f71e646b
Bo Lin <38364537+BoLin@users.noreply.github.com>
2018-09-07 11:13:26 +0800

    Create manual_validation_svm.py

---
+++ AI-Sales/manual_validation_svm.py
@@ -0,0 +1,25 @@
+import numpy as np
+import scipy
+import matplotlib
+from sklearn.neural_network import MLPClassifier
+from sklearn import svm
+import dataloader
+
+X, Y = dataloader.test()
+clf = svm.SVC()
+#clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(62,53,21,16), random_state=1)
+clf.fit(X, Y)
+Y_predict = clf.predict(X)
+Y_predict = Y_predict.tolist()
+
+for i in range(len(Y[0])):
+    correct = 0
+    wrong = 0
+    for j in range(len(Y)):
+        if Y[j][i] == Y_predict[j][i]:
+            correct += 1
+        else:
+            wrong += 1
+    print(i)
+    print(correct/(correct + wrong))
+    print("================")
