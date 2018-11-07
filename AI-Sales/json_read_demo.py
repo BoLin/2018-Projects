8345bb2bc841161c57e9b6bdfa89cd220e11690c
Tired_Bo <38364537+BoLin@users.noreply.github.com>
2018-11-07 17:41:35 +0800

    Delete json_read_demo.py

--- AI-Sales/json_read_demo.py
+++
@@ -1,8 +0,0 @@
-# a demo code to read json file from LabelImg or Labelme
-import json
-with open("demo (10).json") as f: # read demo (10)
-    result = json.load(f)
-for i in range(len(result.get('shapes'))): # read each label from Labelme
-    print(result.get('shapes')[i])
-    
-#print(result.get('annotation').get('object')[1].get('bndbox')) # labelImg file
